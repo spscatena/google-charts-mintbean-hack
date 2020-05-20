4871ece27032b406a86c69acbc8c8e805afca0bf
Sabrina Scatena <sabrina.scatena@gmail.com>
2020-05-20 12:51:57 -0400

    Updates

---
+++ precache-manifest.4deef2fe02b26e01b346fb93d5c47874.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d64de31f8fe4d38ba0ecfb6b9c9af034",
+    "url": "/google-charts-mintbean-hack/index.html"
+  },
+  {
+    "revision": "9b82c5db19097e9758d3",
+    "url": "/google-charts-mintbean-hack/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "49ea3225b6358f569e91",
+    "url": "/google-charts-mintbean-hack/static/js/2.a13f7e7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/google-charts-mintbean-hack/static/js/2.a13f7e7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b82c5db19097e9758d3",
+    "url": "/google-charts-mintbean-hack/static/js/main.38a6dfa2.chunk.js"
+  },
+  {
+    "revision": "b8b46efffb696dd94e75",
+    "url": "/google-charts-mintbean-hack/static/js/runtime-main.5dd4f875.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/google-charts-mintbean-hack/static/media/logo.5d5d9eef.svg"
+  }
+]);
